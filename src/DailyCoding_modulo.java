d4170102bd83032832380cad3232140dbb1c4b06
jinwapp <jinwaap@gmail.com>
2022-06-15 11:37:10 +0900

    수정

--- src/DailyCoding_modulo.java
+++ src/DailyCoding_modulo.java
@@ -19,16 +19,14 @@ public class DailyCoding_modulo {
         int num2 = 67;
         int result = 0;
 
+        if (num2==0){
+            System.out.println("null");
+        }
+
         // while (num1 > 0)
-        while (num1 > 0){
-            // num1 = num1 - num2
+        while (num1 > num2){
             num1 = num1 - num2;
         }
-        if (num1==0){
-            System.out.println("0");
-        } else {
-            result = num1 + num2;
-            System.out.println(result);
-        }
+        System.out.println(num1);
     }
 }
